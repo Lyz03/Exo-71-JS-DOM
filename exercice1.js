ede6f09cf929e3cb0a188f60e213c551ce8bfc56
Lyz03 <lizoe@netcourrier.com>
2021-11-17 17:56:18 +0100

    exo 71

--- exercice1.js
+++ exercice1.js
@@ -1,8 +1,17 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+monDiv.innerText = "Ce div a été ajouté en utilisant javascript";
+
+let secondDiv = document.createElement('div');
+secondDiv.style.backgroundColor = 'red';
+secondDiv.style.width = '100px';
+secondDiv.style.height = '100px';
+
+
+document.getElementById('viewport').appendChild(secondDiv);
 
 document.getElementById('viewport').appendChild(monDiv);
 
-
+document.getElementById('aSupprimer').remove();
 
 
 
